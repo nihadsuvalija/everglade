07f959286671cb63bc0781cf59520b87d3608967
nihadsuvalija <nihadsuvalija@gmail.com>
2022-08-20 14:00:33 +0200

    Minor project change

--- src/com/everglade/main/Panel.java
+++ src/com/everglade/main/Panel.java
@@ -2,6 +2,8 @@ package com.everglade.main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.security.Key;
 
 public class Panel extends JPanel implements Runnable {
     final int originalTileSize = 16;
@@ -13,12 +15,23 @@ public class Panel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+
+    final int fps = 60;
+
     Thread gameThread;
+    KeyHandler keyHandler = new KeyHandler();
+
+    // Players default position:
+    int x = 100;
+    int y = 100;
+    int speed = 4;
 
     public Panel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -34,6 +47,14 @@ public class Panel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/fps;
+        double delta = 0;
+        long initialTime = System.nanoTime();
+        long currentTime;
+
+        long timer = 0;
+        int drawCount = 0;
+
         // This is the game loop!
         while (gameThread != null) {
             /*
@@ -41,23 +62,41 @@ public class Panel extends JPanel implements Runnable {
             * but we also draw the screen with the updated information
             * */
 
-            // Calls the update function every frame:
-            try {
+            currentTime = System.nanoTime();
+            delta += (currentTime - initialTime) / drawInterval;
+            timer += (currentTime - initialTime);
+            initialTime = currentTime;
+
+            if (delta >= 1) {
+                // Calls the update function every frame:
                 update();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+
+                // Draws the graphic on the screen:
+                repaint();
+
+                delta--;
+                drawCount++;
             }
 
-            // Draws the graphic on the screen:
-            repaint();
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
         }
     }
 
     // Function that will call every frame.
-    public void update() throws InterruptedException {
-        // Stops the thread for 20ms, find a better way to cap the framerate of the game!
-        Thread.sleep(20);
-        System.out.println("Game is running!");
+    public void update() {
+        if (keyHandler.upPressed) {
+            y -= speed;
+        } else if (keyHandler.downPressed) {
+            y += speed;
+        } else if (keyHandler.leftPressed) {
+            x -= speed;
+        } else if (keyHandler.rightPressed) {
+            x += speed;
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -65,7 +104,7 @@ public class Panel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.white);
-        g2.fillRect(20,20,tileSize,tileSize);
+        g2.fillRect(x,y,tileSize,tileSize);
         g2.dispose();
     }
 }
